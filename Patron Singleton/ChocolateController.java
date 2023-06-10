c9fc25448a84ef32aa312b38f0ea0c2fd0743709
Erick Cano <ericke.cano@gmail.com>
2023-06-09 17:40:31 -0700

    Practica_5

---
+++ Patron Singleton/ChocolateController.java
@@ -0,0 +1,9 @@
+public class ChocolateController {
+    public static void main(String args[]) {
+		ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+		boiler.fill();
+		boiler.boil();
+		boiler.drain();
+		
+	}
+}
