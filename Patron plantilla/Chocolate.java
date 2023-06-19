b47a44e20c81a61322dbaec3c8b3b1b08da2e107
Erick Cano <ericke.cano@gmail.com>
2023-06-18 17:16:00 -0700

    Practica_10

---
+++ Patron plantilla/Chocolate.java
@@ -0,0 +1,9 @@
+
+public class Chocolate extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Dripping chocolate through filter");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Sugar and Milk");
+	}
+}
